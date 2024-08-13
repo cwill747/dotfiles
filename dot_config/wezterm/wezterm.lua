c034afc607589bdfd46766fdc0092858001af6ae
Cameron Will <stephen.will@tanium.com>
2024-08-13 09:50:50 -0400

    feat: Add split panes to wezterm

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -36,5 +36,44 @@ wezterm.on('update-status', function(window)
   }))
 end)
 
+config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 }
+
+local function move_pane(key, direction)
+  return {
+    key = key,
+    mods = 'LEADER',
+    action = wezterm.action.ActivatePaneDirection(direction),
+  }
+end
+
+config.keys = {
+  {
+    -- I'm used to tmux bindings, so am using the quotes (") key to
+    -- split horizontally, and the percent (%) key to split vertically.
+    key = '"',
+    -- Note that instead of a key modifier mapped to a key on your keyboard
+    -- like CTRL or ALT, we can use the LEADER modifier instead.
+    -- This means that this binding will be invoked when you press the leader
+    -- (CTRL + A), quickly followed by quotes (").
+    mods = 'LEADER',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = '%',
+    mods = 'LEADER',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+    {
+    key = 'a',
+    -- When we're in leader mode _and_ CTRL + A is pressed...
+    mods = 'LEADER|CTRL',
+    -- Actually send CTRL + A key to the terminal
+    action = wezterm.action.SendKey { key = 'a', mods = 'CTRL' },
+  },
+  move_pane('j', 'Down'),
+  move_pane('k', 'Up'),
+  move_pane('h', 'Left'),
+  move_pane('l', 'Right'),
+}
 -- Returns our config to be evaluated. We must always do this at the bottom of this file
 return config
