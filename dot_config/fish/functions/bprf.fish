8ff2492a7e3116133f91701b79fc8c614d100750
Cameron Will <stephen.will@tanium.com>
2025-05-21 18:01:28 +0000

    fix: Update spacing on bprs

--- dot_config/fish/functions/bprf.fish
+++ dot_config/fish/functions/bprf.fish
@@ -1,17 +1,18 @@
 function bprf
-  set upstreambranch "origin/release/$relbranch"
-  set base "release/$relbranch"
-  if not git branch -r --list | grep -q $upstreambranch
-    echo "Project doesn't use slashes, checking dashes"
-    set upstreambranch "origin/release-$relbranch"
-    set base "release-$relbranch"
-  end
-  echo "Set upstream branch to $upstreambranch"
-  git cherry-pick --continue
-  set commit_title (git log -1 --pretty=format:%s)
-  set commit_body (git log -1 --pretty=format:%B)
-  set pr_title "main->$relbranch: $commit_title"
-  set pr_title (string trim --chars 60 $pr_title)
-  gh pr create --title "$pr_title" --base "$base"
-  set -e relbranch
+    set upstreambranch "origin/release/$relbranch"
+    set base "release/$relbranch"
+    if not git branch -r --list | grep -q $upstreambranch
+        echo "Project doesn't use slashes, checking dashes"
+        set upstreambranch "origin/release-$relbranch"
+        set base "release-$relbranch"
+    end
+    echo "Set upstream branch to $upstreambranch"
+    git cherry-pick --continue --no-commit
+    git commit --no-verify
+    set commit_title (git log -1 --pretty=format:%s)
+    set commit_body (git log -1 --pretty=format:%B)
+    set pr_title "main->$relbranch: $commit_title"
+    set pr_title (string trim --chars 60 $pr_title)
+    gh pr create --title "$pr_title" --base "$base"
+    set -e relbranch
 end
